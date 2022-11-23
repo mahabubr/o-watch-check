bf2c9ba1efbe857a3b4076ba69ed7ce12ebeb740
mahabubr <mahabub.r753@gmail.com>
2022-11-23 21:33:00 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [{
+      mytheme: {
+        primary: "#6ECCAF",
+        secondary: "#D23369",
+        accent: "#009EFF",
+        neutral: "#F49D1A",
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+        "base-100": "#FFFFFF",
+      }
+    }],
+  },
+}
