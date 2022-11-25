ed9228a234e311f927bcab5067e272d254a07fd1
mahabubr <mahabub.r753@gmail.com>
2022-11-25 17:29:14 +0600

    my booking product show

--- src/Pages/Page/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Page/Dashboard/MyOrders/MyOrders.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
+import Loader from '../../../../Components/Loader/Loader';
+import { AuthContext } from '../../../../Contexts/AuthProvider/AuthProvider';
 
 const MyOrders = () => {
+
+    const { user } = useContext(AuthContext)
+
+    const { data: myOrdersList, isLoading } = useQuery({
+        queryKey: ['my-orders', user?.email],
+        queryFn: () => fetch(`http://localhost:5000/my-orders/?email=${user?.email}`)
+            .then(res => res.json())
+            .catch(e => toast.error(e.message))
+    })
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    // order having an image, title, price, and a pay button
+
     return (
-        <div>
-            MyOrders
+        <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Payment Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        myOrdersList.map(order =>
+                            <tr key={order._id}>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-20 h-20">
+                                                <img src={order.booking_image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {order.booking_title}
+                                    <br />
+                                    <span className="badge badge-secondary p-3 text-white badge-sm">{order.booking_category_name}</span>
+                                </td>
+                                <td>$ {order.booking_price}</td>
+                                <th>
+                                    <button className="btn btn-primary btn-sm">Pay Now</button>
+                                </th>
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Pages/Page/Home/BookNowModal/BookNowModal.js
+++ src/Pages/Page/Home/BookNowModal/BookNowModal.js
@@ -1,22 +1,51 @@
 import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import Swal from 'sweetalert2';
 import { AuthContext } from '../../../../Contexts/AuthProvider/AuthProvider';
 
 const BookNowModal = ({ singleWatch, setSingleWatch }) => {
 
-    const { product_name, product_resale_price } = singleWatch
+    const { product_name, product_resale_price, product_pic, category_name, _id } = singleWatch
 
     const { user } = useContext(AuthContext)
 
     const handleSubmitBooing = (event) => {
         event.preventDefault()
 
-        setSingleWatch(null)
-        Swal.fire(
-            'Congratulations',
-            'Your Booking Successfully Done',
-            'success'
-        )
+        const location = event.target.location.value
+        const number = event.target.number.value
+
+        const myBookingInfo = {
+            booking_image: product_pic,
+            booking_title: product_name,
+            booking_price: product_resale_price,
+            booking_user_email: user?.email,
+            booking_user_location: location,
+            booking_user_number: number,
+            booking_category_name: category_name,
+            booking_item_id: _id
+        }
+
+        fetch('http://localhost:5000/my-orders', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(myBookingInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    setSingleWatch(null)
+                    Swal.fire(
+                        'Congratulations',
+                        'Your Booking Successfully Done',
+                        'success'
+                    )
+                }
+            })
+            .catch(e => toast.error(e.message))
+
     }
 
     return (
@@ -30,8 +59,8 @@ const BookNowModal = ({ singleWatch, setSingleWatch }) => {
                         <input type="text" defaultValue={user?.displayName} readOnly className="input input-bordered input-info w-full" />
                         <input type="text" defaultValue={user?.email} readOnly className="input input-bordered input-info w-full" />
                         <input type="text" defaultValue={`$ ${product_resale_price}`} readOnly className="input input-bordered input-info w-full" />
-                        <input type="number" placeholder='Enter Your Phone Number' className="input input-bordered input-info w-full" required />
-                        <input type="text" placeholder='Enter Your Location' className="input input-bordered input-info w-full" required />
+                        <input type="number" name='number' placeholder='Enter Your Phone Number' className="input input-bordered input-info w-full" required />
+                        <input type="text" name='location' placeholder='Enter Your Location' className="input input-bordered input-info w-full" required />
                     </div>
                     <input type="submit" value="Submit Your Booking" data-mdb-ripple="true"
                         data-mdb-ripple-color="light"
